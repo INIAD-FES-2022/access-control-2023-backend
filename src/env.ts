70a6697bb7529e2a1b226664e49d29158c612823
yu7400ki <yu7400ki@gmail.com>
2023-09-21 01:56:59 +0900

    znv

---
+++ src/env.ts
@@ -0,0 +1,9 @@
+import { parseEnv } from "znv";
+import { z } from "zod";
+
+export const env = parseEnv(process.env, {
+  PORT: z.number().default(8080),
+  NODE_ENV: z
+    .enum(["development", "production", "test"])
+    .default("development"),
+});
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,11 @@
 import { serve } from "@hono/node-server";
+import { env } from "env";
 import { Hono } from "hono";
 
 const app = new Hono();
 app.get("/", (c) => c.text("Hello Hono!"));
 
-serve(app);
+serve({
+  fetch: app.fetch,
+  port: env.PORT,
+});
