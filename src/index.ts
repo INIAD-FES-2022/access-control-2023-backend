669e5db4822cb20e98fa00f5ed8fe636c95355dc
yu7400ki <yu7400ki@gmail.com>
2023-09-21 02:08:05 +0900

    logger

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,11 @@
 import { serve } from "@hono/node-server";
 import { env } from "env";
 import { Hono } from "hono";
+import { logger } from "hono/logger";
 
 const app = new Hono({ strict: false });
+
+app.use("*", logger());
 app.get("/", (c) => c.text("Hello Hono!"));
 
 serve({
