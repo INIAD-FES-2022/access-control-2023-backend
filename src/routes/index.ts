1fea6a3562dd05d8e09e2d921a04840bfd43c6f3
yu7400ki <yu7400ki@gmail.com>
2023-09-25 18:41:11 +0900

    remove demo route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,11 +1,9 @@
 import { OpenAPIHono } from "@hono/zod-openapi";
-import demoHandler from "./demo";
 import enterHandler from "./enter";
 import userHandler from "./user";
 
 const handler = new OpenAPIHono();
 
-handler.route("/demo", demoHandler);
 handler.route("/user", userHandler);
 handler.route("/enter", enterHandler);
 
